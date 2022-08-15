4a8f4a8449596920811e1cfd9bdca224a0e593a7
Hakimix <mustafa.hakimi@outlook.dk>
2022-08-15 12:03:34 +0200

    enable nun-stack

--- app.py
+++ app.py
@@ -5,14 +5,19 @@ import aws_cdk as cdk
 
 from nun.lambda_stack import LambdaStack
 from nun.sqs_stack import SQSStack
+from nun.nun_stack import NunStack
 
 app = cdk.App()
 
+nun_stack = NunStack(app, 'nun-stack')
+
+""" Separated Stacks 
 lambda_stack = LambdaStack(app, 'lambda-stack')
 
 sqs_stack = SQSStack(app, 'sqs-stack', lambda_fns={
   'lambda_pull': lambda_stack.lambda_pull,
   'lambda_mod': lambda_stack.lambda_mod
 })
+"""
 
 app.synth()
